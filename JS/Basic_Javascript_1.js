c4ac7a56a1d1a4aa51a443cd78523148f66373bb
Jared Hubbard <hubbard.abstract@gmail.com>
2020-04-01 13:43:25 -0700

    javascript basic learn stuff

---
+++ JS/Basic_Javascript_1.js
@@ -0,0 +1,16 @@
+window.alert("Hellow World!");
+document.write("Hello from the other side!")
+var A= "This is a string";
+document.write(A);
+document.write("Lisa told Bart, \"Knock it off, Bart! or i'll tell dad!\"<br> \"Eat my shorts!\" Bart responded.")
+
+var B ="\I pity the man bound to his crown of ashes,"
++ "For he is left to rule an empty kingdom\ barren and cracked.\""
++"-Jared H";
+document.write(B)
+
+var reds = "I am mad.";
+var reds = reds.fontcolor("red");
+document.write(reds);
+
+document.write(210+210);
